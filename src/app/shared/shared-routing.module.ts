1b94729cd04afd25f40869a0971857a66b2ab154
mechztcc <dojathan@gmail.com>
2021-09-14 14:52:56 -0300

    changes

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionsGuard } from '../core/guards/sessions.guard';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent, canActivate: [SessionsGuard] }
 ];
 
 @NgModule({
